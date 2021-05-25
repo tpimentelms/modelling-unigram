96cf1b2a12234de5659a4e6630a53f8a88edc663
Tiago Pimentel <tiagopms@gmail.com>
2021-05-25 11:37:48 +0100

    Fix function spacing

--- src/util/argparser.py
+++ src/util/argparser.py
@@ -7,11 +7,13 @@ def add_all_defaults(parser):
     add_optimisation_args(parser)
     add_generator_args(parser)
 
+
 def add_optimisation_args(parser):
     # Optimization
     parser.add_argument('--epochs', type=int, default=5)
     parser.add_argument('--eval-batches', type=int, default=200)
     parser.add_argument('--wait-epochs', type=int, default=5)
+
 
 def add_data_args(parser):
     # Data defaults
@@ -19,6 +21,7 @@ def add_data_args(parser):
     parser.add_argument('--data-file', type=str, required=True)
     parser.add_argument('--batch-size', type=int, default=32)
     parser.add_argument('--dataset', type=str)
+
 
 def add_generator_args(parser):
     # Model defaults
@@ -28,11 +31,13 @@ def add_generator_args(parser):
     parser.add_argument('--hidden-size', type=int, default=512)
     parser.add_argument('--dropout', type=float, default=.33)
 
+
 def get_argparser():
     parser = argparse.ArgumentParser(description='LanguageModel')
     parser.add_argument('--seed', type=int, default=7,
                         help='Seed for random algorithms repeatability (default: 7)')
     return parser
+
 
 def parse_args(parser):
     args = parser.parse_args()
